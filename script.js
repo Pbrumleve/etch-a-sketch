944add4c90f8a5d4ccc689866e39432ff9b6efa0
Paul Brumleve <paul.brumleve@gmail.com>
2022-10-06 16:40:43 -0500

    Created working restart button.

--- script.js
+++ script.js
@@ -13,6 +13,30 @@ squares.forEach((div) => {
   div.addEventListener('mouseover', toggleColor)
 })
 
+function remakeGrid() {
+  let sideLengthString = prompt('How many squares per side?', '');
+  if (sideLengthString === null) {
+    return;
+  } else {
+    let sideLength = Number(sideLengthString);
+    if (sideLength > 0 && sideLength <= 100 && Number.isInteger(sideLength)) {
+      while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+      for (let i = 0; i < sideLength ** 2; i++) {
+          let li = document.createElement('div');
+          li.classList.add('grid');
+          li.classList.add('whiteOne');
+          li.setAttribute('id', `square-${i+1}`);
+          container.appendChild(li);
+        }
+      document.getElementById("box").style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`;
+    } else {
+      return alert(`Please try again with an integer between 1 and 100.\nOtherwise, the game might not work.`)
+    }
+  }
+}
+
 function toggleColor() {
   let squareColor = this.classList;
   console.log(squareColor);
@@ -50,37 +74,3 @@ function toggleColor() {
     return;
   }
 }
-
-function remakeGrid() {
-  let sideLengthString = prompt('How many squares per side?', '');
-  let sideLength = Number(sideLengthString);
-  let row;
-  let column;
-  if (Number.isInteger(sideLength) && 0 < sideLength <= 100) {
-    while (container.firstChild) {
-      container.removeChild(container.lastChild);
-    }
-    for (let i = 0; i < sideLength; i++) {
-      row = document.createElement('div');
-      row.classList.add('row');
-      row.setAttribute('id', sideLength * i);
-      container.appendChild(row);
-    }
-    const rows = document.querySelectorAll('.row');
-    rows.forEach((row) => {
-      for (let i = 0; i < sideLength; i++) {
-        column = document.createElement('div');
-        column.classList.add('grid');
-        column.classList.add('whiteOne');
-        column.setAttribute('id', `square-${i+1+Number(row.id)}`)
-        row.appendChild(column);
-      }
-    })
-  } else {
-    return alert(`Please try again with an integer between 1 and 100.\nOtherwise, the game might not work.`)
-  }
-}
-
-function square(x) {
-    return (x * x);
-  }
